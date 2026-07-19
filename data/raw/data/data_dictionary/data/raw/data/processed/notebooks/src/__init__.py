a03b6e6a01a1f73ba57f4a0e441ff810002f8859
TINKU001 <38125081+TINKU001@users.noreply.github.com>
2026-07-19 12:11:04 +0530

    Create __init__.py

---
+++ data/raw/data/data_dictionary/data/raw/data/processed/notebooks/src/__init__.py
@@ -0,0 +1,6 @@
+"""
+AI-Driven Early Cardiovascular Disease Risk Prediction
+for Improved Clinical Decision Support in Health Insurance Underwriting.
+
+Walsh University AI/ML Capstone Project.
+"""
